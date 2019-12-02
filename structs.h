f6e7f997cd3835e54ce1e139e3fbdcb8cd2e5d8b
jwhall3 <58199173+jwhall3@users.noreply.github.com>
2019-12-02 17:19:01 -0500

    Update structs.h

--- structs.h
+++ structs.h
@@ -1,17 +1,22 @@
+#pragma once
 /*****************************************************************
 * Header file to use in your implementation of the project.      *
 * Please, do not modify any value or structure that appears here *
 *	
-//References:							 *
-https://www.geeksforgeeks.org/structure-member-alignment-padding-and-data-packing/
+//References:							
 *****************************************************************/
 #include <stdio.h>
 
+using namespace std;
+
 struct secretKey {
-    unsigned int secKey;
-    unsigned short requestType;
+    //secretKey is the secret key contained in the request.
+    unsigned int secKey;   
+    //newKey (0), fileGet (1), fileDigest (2), fileRun (3).
+    unsigned short requestType; 
     unsigned short padding;
 };
-//Struct Types
 
+
+//Struct Types
 
