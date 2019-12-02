60aa6b55f197ab10a94b71cddf1a8234618aefe7
Blake <blake.sweet16@gmail.com>
2019-12-02 15:37:03 -0500

    Added structs file
    This file will be included between client and server. Includes structures and constants for use between communication.

---
+++ structs.h
@@ -0,0 +1,11 @@
+/*****************************************************************
+* Header file to use in your implementation of the project.      *
+* Please, do not modify any value or structure that appears here *
+*								 *
+* Special notations:						 *
+*	- Authentication Server = AS				 *
+*	- Application Server = AP			       	 *
+*****************************************************************/
+#include <stdio.h>
+
+//Struct Types
