086618788c7b68c9a08c83187a4b996aaee80f19
jwhall3 <58199173+jwhall3@users.noreply.github.com>
2019-12-02 18:19:46 -0500

    Update structs.h

--- structs.h
+++ structs.h
@@ -6,17 +6,24 @@
 //References:							
 *****************************************************************/
 #include <stdio.h>
+#include <string>
+#include <iostream>
 
 using namespace std;
 
 struct secretKey {
     //secretKey is the secret key contained in the request.
-    unsigned int secKey;   
+    unsigned int secKey = 123456;
     //newKey (0), fileGet (1), fileDigest (2), fileRun (3).
     unsigned short requestType; 
     unsigned short padding;
 };
 
+struct host {
+    string h_name;		//official name of host 
+    int h_addrtype;		//host address type, always AF_INET
+	int h_length;		//length of address in bytes
+}
 
 //Struct Types
 
