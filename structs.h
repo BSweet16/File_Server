25a1a81704459fd937d0f8b1c92aa78babd45979
jwhall3 <58199173+jwhall3@users.noreply.github.com>
2019-12-02 17:01:12 -0500

    added secretKey struct
        unsigned int secKey;
        unsigned short requestType;
        unsigned short padding;

--- structs.h
+++ structs.h
@@ -1,11 +1,17 @@
 /*****************************************************************
 * Header file to use in your implementation of the project.      *
 * Please, do not modify any value or structure that appears here *
-*								 *
-* Special notations:						 *
-*	- Authentication Server = AS				 *
-*	- Application Server = AP			       	 *
+*	
+//References:							 *
+https://www.geeksforgeeks.org/structure-member-alignment-padding-and-data-packing/
 *****************************************************************/
 #include <stdio.h>
 
+struct secretKey {
+    unsigned int secKey;
+    unsigned short requestType;
+    unsigned short padding;
+};
 //Struct Types
+
+
