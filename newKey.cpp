c0ffd7ac1a9b043d8bc4c3d01730fd67d93a2550
jwhall3 <58199173+jwhall3@users.noreply.github.com>
2019-12-03 00:06:59 -0500

    Update newKey.cpp

--- newKey.cpp
+++ newKey.cpp
@@ -1,79 +1,97 @@
+#include "structs.h"
 #include <string>
 #include <cstring>
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <iostream>
-#include <string.h>
 #include <sys/types.h>
-#include <sys/socket.h> // sockaddr_in 
-#include <netinet/in.h>
+#include <sys/socket.h>
+#include <netinet/in.h> //defines the sockaddr_in struct
 #include <netdb.h>      //gethostbyname(), struct hostent
 #include <iostream>
-#include "structs.h"
 
 //#define SERVER "localhost"
 //#define PORT "2100"
-
-#define MAXDATASIZE 100 // max number of bytes we can get at once 
+#define MAXDATASIZE 107
 
 using namespace std;
 
 //prints error message, then exits
 void error(const char *msg)
 {
-	perror(msg);
-	exit(0);
+    perror(msg);
+    exit(0);
 }
 
+int main(int argc, char *argv[])
+{
 
-     
- 
-        
-}
-
-
-int main(int argc, char *argv[]){
-    
     unsigned int numArgs = argc;
-    if (numArgs != 5){
-        error("Incorrect number of arguments\n");
+    if (numArgs != 5)
+    {
+        error("failure\n");
     }
-    
+
     int socketfd;
-    int portnum;
-    int n;
+    int retVal;
     //will contain the address of the server to which we want to connect
     struct sockaddr_in server_addr;
+    //information to send to
+    struct clientToServer cS;
     //http://www.linuxhowtos.org/C_C++/socket.htm
-    struct hostent *server;
+    //struct hostent *server;
     //returns pointer to hostent constaining information about that host, NULL = couldnt locate host
-	server = gethostbyname(argv[1]);
-   
-    if (server == NULL) {
-		fprintf(stderr, "ERROR, no such host\n");
-		exit(0);
-	}
+    //server = gethostbyname(argv[1]);
 
-    unsigned int portNum = atoi(argv[2]);
-    unsigned int secKey = atoi(argv[3]);
-    unsigned int newSecKey =atoi(argv[4]);
+    string hostName = argv[1]; //should this be defined or is it dynamic, how is this tested?
+    unsigned short portNum = atoi(argv[2]);
+
+    //load up struct from cmd arguments
+    cS.secKey = atoi(argv[3]);
+    //request type is 0 for changing secret key
+    cS.requestType = ntohs(0);
+    //arbitary numbers for padding
+    cS.padding = ntohs(11);
+    cS.details = atoi(argv[4]);
+   
+
+    //creating socket
+    socketfd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    if (socketfd == -1)
+    {
+        error("failure\n");
+    }
+    //clear out server_addr
+    bzero(&server_addr, sizeof(server_addr)); 
+
     //This code sets the fields in the  serv_addr struct
     server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(portNum); //htons() = host to network byte order
-    server_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
-
-    //connect() called by client to connect to server 
-	//--(socket fd, & of host w/ portnum, sizeof this address)
-	//Notice that the client needs to know the port number of the server, 
-	//but it does not need to know its own port number.
-	if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		error("ERROR connecting");
-	}
+    server_addr.sin_port = htons(portNum);         //htons() = host to network byte order
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1"); //not sure what to put here, this may be correct
 
 
+    //connect() called by client to connect to server
+    //--(socket fd, & of host w/ portnum, sizeof this address)
+    //Notice that the client needs to know the port number of the server,
+    //but it does not need to know its own port number.
+    if (connect(socketfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        error("failure\n");
+    }
     
-    
+    char buffer[MAXDATASIZE];
+    //concatonate struct values for transmission
+    sprintf(buffer, "%d%d%d%d", cS.secKey, cS.requestType, cS.padding, cS.details); 
+
+    n = write(socketfd, buffer, strlen(buffer));
+    if (n < 0){
+		error("ERROR writing to socket");
+    }
+
+	bzero(buffer, 256);
+    close(socketfd);
+	return 0;
 
 }
-    
