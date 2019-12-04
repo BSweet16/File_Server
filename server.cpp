0e174a0b7f8b62ead8ba14c5f4e9b0ef97dc742a
Blake <blake.sweet16@gmail.com>
2019-12-04 14:32:34 -0500

    Update basic structure

--- server.cpp
+++ server.cpp
@@ -6,7 +6,29 @@
 //  Copyright © 2019 Blake Sweet. All rights reserved.
 
 #include <iostream>
+#include "struct.h"
 using namespace std;
 
+void printOutput(unsigned int secretKey, short requestType){
+	cout << "Secret key: " << secretKey << '\n'
+		 << "Request Type: " << requestType << '\n';
+}
+
 int main(int argc, const char * argv[]) {
+	// Local variables
+	unsigned int secretKey;
+	short requestType;
+	const char BUFFER_PADDING[2] = {0x00, 0x00}; // 2 bytes of padding (arbitrary 0's)
+
+	// Keep in loop
+		//undo network byte order
+			// store as secret key
+
+		// Test for correct number of arguments
+		// Create socket for sending and recieving data (Structs)
+		// Construct local address structure
+		// Bind to local address
+			// Interpret data
+			// Print data
+			printOutput(secretKey, requestType);
 }
