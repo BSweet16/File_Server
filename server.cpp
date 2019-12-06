dac7a0d67423177e77295094189df1fe1d7b6595
Blake <blake.sweet16@gmail.com>
2019-12-06 14:38:37 -0500

    Starting over on server.cpp, wrong idea

--- server.cpp
+++ server.cpp
@@ -9,6 +9,7 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include "structs.h"
+
 using namespace std;
 
 void PrintOutput(unsigned int secretKey, short requestType, unsigned int recMessageSize, unsigned int clientAddress, unsigned int listenAddress){
@@ -27,68 +28,18 @@ void DieWithError(string errorMessage){
 
 int main(int argc, const char * argv[]) {
 	// Ex) filed 5678 987654 (5678 = port number, 987654 = secret key) 
-	// if (argc != 3){
-	// 	perror(argv[0]);
-	// 	exit(1);
-	// }
-
-	// Local variables
-	char buff[MAXDATASIZE+8]; // = Data + secretKey
-	unsigned int listenAddress;
-	unsigned int clientAddress;
-	unsigned int port; //= argv[2]
-
-	unsigned int secretKey; //= argv[3] // Like a PIN
-	short requestType;
-	unsigned int counter = 0;
-	unsigned int cliAddrLen; // Length of incoming message
-	unsigned int recMessageSize; // Size of recieved message (bytes)
-
-	// Listen for information
-	while (counter < MAXDATASIZE){
-		//buff[counter];// = recieved data;
-
-		// Create socket for sending/recieving
-		if ((sock = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0){
-			DieWithError("socket() failed");
-		}
-
-		/* ==========================
-    	*	Construct local address structure
-    	   ========================== */
-   		memset(&echoServAddr, 0, sizeof(echoServAddr));   // Zero out structure
-    	echoServAddr.sin_family = AF_INET;                // Internet address family
-    	echoServAddr.sin_addr.s_addr = htonl(INADDR_ANY); // Any incoming interface
-    	echoServAddr.sin_port = htons(echoServPort);      // Local port
-
-    	/* ==========================
-    	*	Bind to the local address
-    	   ========================== */ 
-		// Test for errors
-	    if (bind(sock, (struct sockaddr *) &echoServAddr, sizeof(echoServAddr)) < 0){
-	        DieWithError("bind() failed");
-	    } 
-	    cliAddrLen = sizeof(clientAddress);
-	    if ((recMessageSize = recvfrom(sock, echoBuffer, MAXDATASIZE, 0, &clientAddress, &cliAddrLen)) < 0){
-	    	DieWithError("recvfrom() failed");
-	    }       
-
-	    // Mark the end of the message
-	    cout << "Handling client " << inet_ntoa(clientAddress) << '\n';
-	    buff[recMessageSize] = '\0';
-	    cout << "Recieved: " << buff << '\n';
-
-	    // Define variables for recieving information
-	    recMessageSize = sizeof(buff);
-
-	    // Recieve message
-	    if ((recvMsgSize = recvfrom(listenAddress, &buff, recMessageSize, 0, &clientAddress, &cliAddrLen)) < 0){
-	    	DieWithError("recvfrom() failed");
-	    }
-
-	   	// Output
-		PrintOutput(secretKey, requestType, recMessageSize, clientAddress, listenAddress);
-
-		counter++;
+	if (argc > 3){
+		perror(argv[0]);
+		exit(1); // Error
 	}
+	else if (argc < 3){
+		cout << "Correct usage: ./[ServerName] [Port Number] [Secret Key]\n";
+		exit(0); // Normal exit type
+	}
+
+	// Local Variables
+	unsigned int key;
+	short type;
+
+	rio_readn();
 }
