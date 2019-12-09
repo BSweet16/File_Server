033865d2a0f3656da2004ea40d2ba383ee05cfd1
jwhall3 <58199173+jwhall3@users.noreply.github.com>
2019-12-09 13:56:02 -0500

    replaced all files

---
+++ common.cpp
@@ -0,0 +1,65 @@
+/*****************************************************************
+* Header file containing constants for the "library"             *
+*****************************************************************/
+extern "C" {
+#include "csapp.h"
+}
+
+#include <stdio.h>
+using namespace std;
+
+// Const values
+const char BUFFER_PADDING[2] = { 0x00, 0x00 }; // 2 bytes of padding (arbitrary 0's)
+const unsigned int MAXDATASIZE = 100;
+
+// Return 0 on success, -1 on failure
+int newKey(const char *machineName, unsigned short port, unsigned int secretKey, unsigned int newKey) {
+
+	int answer = 0; 
+	rio_t rio;
+	int clientfd;
+
+	//change info to network byte order
+	unsigned int secKey = htonl(secretKey);
+	unsigned short type = htons(0);
+	unsigned int formattedNewKey = htonl(newKey);
+
+	clientfd = Open_clientfd(machineName, port);
+	Rio_readinitb(&rio, clientfd);
+
+	//send seckey for validation from server
+	Rio_writen(clientfd, secKey, sizeof(secKey);
+	//tell server what type of request this is
+	Rio_writen(clientfd, type, sizeof(type);
+
+	Rio_readn(clientfd, answer, sizeof(answer));
+	if (answer == -1) {
+		return answer;
+	}
+	else {
+		
+		Rio_writen(clientfd, BUFFER_PADDING, sizeof(BUFFER_PADDING);
+		Rio_writen(clientfd, formattedNewKey, sizeof(formattedNewKey);
+		Close(clientfd);
+		return answer;
+	}
+}
+
+int fileGet(const char *machineName, unsigned int port, unsigned int secretKey, const char *fileName, char *result, unsigned int *resultLength) {
+	int answer;
+
+	// Code
+
+	return answer;
+}
+
+int fileDigest(char *machineName, unsigned short port, unsigned int secretKey, const char *fileName, char *result, unsigned int *resultLength) {
+	int answer;
+
+	// Code
+
+	return answer;
+}
+
+// Extra Credit
+int fileRun(const char *machineName, unsigned short port, unsigned int secretKey, const char *request, char *result, unsigned int *resultLength);
---
+++ filed.cpp
@@ -0,0 +1,100 @@
+//
+//  main.cpp
+//  Server
+//
+//  Created by Blake Sweet on 10/28/19.
+//  Copyright © 2019 Blake Sweet. All rights reserved.
+
+#include <iostream>
+#include <csignal>
+#include <stdio.h>
+#include <string>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include "common.cpp"
+#include "csapp.h"
+
+void printOutput(unsigned int secretKey, short requestType, string detail, int completion) {
+	
+	cout << "Secret Key = " << secretKey << endl;
+	cout << "Request Type = " << requestType << endl;
+	cout << "Detail = " << detail << endl;
+	cout << "Completion = " << completion << endl;
+	cout << "--------------------------" << endl;
+
+}
+
+using namespace std;
+
+
+int main(int argc, const char * argv[]) {
+	int listenfd, connfd, port;
+	socklen_t clientlen;
+	struct sockaddr_in clientaddr;
+	struct hostent *hp;
+	char *haddrp;
+	unsigned int secKey = 123456;
+	string dataDetail;
+	int status = 0;
+	unsigned short reqType;
+
+	if (argc > 3) {
+		perror(argv[0]);
+		exit(1); // Error
+	}
+	else if (argc < 3) {
+		cout << "Correct usage: ./[ServerName] [Port Number] [Secret Key]\n";
+		exit(0); // Normal exit type
+	}
+
+	port = atoi(argv[1]);
+	testKey = atoi(argv[2]);
+
+	if (testKey != secKey) {
+		exit(0)
+	}
+
+	listenfd = Open_listenfd(port);
+	clientlen = sizeof(clientaddr);
+
+	connfd = Accept(listenfd, (SA *)&clientaddr, &clientlen);
+
+	/* Determine the domain name and IP address of the client */
+	hp = Gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr,
+		sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+	haddrp = inet_ntoa(clientaddr.sin_addr);
+
+	//printf("server connected to %s (%s)\n", hp->h_name, haddrp);
+
+	unsigned int clientKey;
+	//readin key sent from client to verify
+	Rio_readn(listenfd, clientKey, sizeof(clientKey));
+	//check request type
+	
+	Rio_readn(listenfd, reqType, sizeof(reqType));
+	//ntohl() - to convert from network to host order
+	if (clientKey != secKey) {
+		int rVal = -1;
+		Rio_writen(listenfd, rVal, sizeof(rVal));
+		close(listenfd);
+		printOutput(secKey, reqType, dataDetail, status);
+	}
+	//if key is valid, process the rest of client info
+	else{
+		if (reqType == 0) {
+			//read in buffer?
+			char BUFFER_PADDING[2];
+			Rio_readn(listenfd, BUFFER_PADDING, sizeof(BUFFER_PADDING));
+			//read in new secret key
+			unsigned int newKeyfromClient;
+			Rio_readn(listenfd, newKeyfromClient, sizeof(newKeyfromClient));
+			dataDetail = newKeyfromClient;
+			printOutput(secKey, reqType, dataDetail, status);
+			secKey = newKeyfromClient;
+		}
+		else if (reqType == 1) {
+			//Rio_readn(listenfd, )
+		}
+	}
+	return 0;
+} // End of main
